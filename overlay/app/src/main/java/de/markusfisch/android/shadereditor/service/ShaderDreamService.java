77ff994521661e9a6bc0c31105be8534937133e8
appym005 <apoorvmishra05@gmail.com>
2026-08-14 11:19:51 +0530

    Add Shader Editor screen saver support
    Validated in GitHub Actions: patch application, Android API 36 setup, debug APK build, artifact upload, and publication of full modified source all succeeded.

---
+++ overlay/app/src/main/java/de/markusfisch/android/shadereditor/service/ShaderDreamService.java
@@ -0,0 +1,75 @@
+package de.markusfisch.android.shadereditor.service;
+
+import android.opengl.GLSurfaceView;
+import android.service.dreams.DreamService;
+
+import de.markusfisch.android.shadereditor.app.ShaderEditorApp;
+import de.markusfisch.android.shadereditor.database.DataRecords;
+import de.markusfisch.android.shadereditor.database.DataSource;
+import de.markusfisch.android.shadereditor.database.Database;
+import de.markusfisch.android.shadereditor.widget.ShaderView;
+
+/**
+ * Android screen saver (DreamService) that renders the shader selected in
+ * Shader Editor's "Set as screen saver" action.
+ */
+public class ShaderDreamService extends DreamService {
+	private ShaderView view;
+
+	@Override
+	public void onAttachedToWindow() {
+		super.onAttachedToWindow();
+		setInteractive(false);
+		setFullscreen(true);
+
+		view = new ShaderView(this, GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+		setContentView(view);
+		setShader();
+	}
+
+	@Override
+	public void onDreamingStarted() {
+		super.onDreamingStarted();
+		if (view != null) {
+			view.onResume();
+		}
+	}
+
+	@Override
+	public void onDreamingStopped() {
+		if (view != null) {
+			view.onPause();
+		}
+		super.onDreamingStopped();
+	}
+
+	@Override
+	public void onDetachedFromWindow() {
+		if (view != null) {
+			view.onPause();
+			view = null;
+		}
+		super.onDetachedFromWindow();
+	}
+
+	private void setShader() {
+		DataSource dataSource = Database.getInstance(this).getDataSource();
+
+		long shaderId = ShaderEditorApp.preferences.getScreensaverShader();
+		DataRecords.Shader shader = dataSource.shader.getShader(shaderId);
+
+		if (shader == null) {
+			shader = dataSource.shader.getRandomShader();
+			if (shader == null) {
+				return;
+			}
+			ShaderEditorApp.preferences.setScreensaverShader(shader.id());
+		}
+
+		if (view != null) {
+			view.getRenderer().setFragmentShader(
+					shader.fragmentShader(),
+					shader.quality());
+		}
+	}
+}
